444dbf80a47c13fc5546debe17a767a344d48f4a
Al-Farhaad <mohammadalt786@gmail.com>
2024-08-24 01:49:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {
+  Hero,
+  PopularProducts,
+  SpecialOffer,
+  SuperQuality,
+  CustomerReviews,
+  Services,
+  Subscribe,
+  Footer,
+} from "./sections";
+import Navbar from "./components/Navbar";
+
+const App = () => (
+  <main className="relative">
+    <Navbar/>
+    <section className="xl:padding-l wide:padding-r padding-b"><Hero/></section>
+    <section className="padding"><PopularProducts/></section>
+    <section className="padding"><SuperQuality/></section>
+    <section className="padding-x padding-y-10"><Services/></section>
+    <section className="padding"><SpecialOffer/></section>
+    <section className="bg-pale-blue padding"><CustomerReviews/></section>
+    <section className="padding-x sm:py-32 py-16 w-full"><Subscribe/></section>
+    <section className="bg-black padding-x padding-t pb-8"><Footer/></section>
+  </main>
+);
+
+export default App;
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    CustomerReviews,
+    Services,
+    Footer
+}
